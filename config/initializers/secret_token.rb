802105eeae12e552ba60f39ce48c774cc5892f4a
Ted Lilley <tlilley@diditbetter.com>
2012-11-29 10:28:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeDibs::Application.config.secret_token = '81e44e532b111293cda2f68e9d2951b0229926f3df2962d9e7fc1fc095402f1ef35a72a49bf110966b0be2188ba8ae4803ccf1e35c9e175cd7df0b8ddf467c38'
