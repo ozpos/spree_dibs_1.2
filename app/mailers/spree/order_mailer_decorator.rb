c3f7ce616fa0aa063f9210d298662df61d274497
Ted Lilley <ted.lilley@gmail.com>
2012-12-03 02:19:48 -0500

    Add html email versions

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -3,27 +3,39 @@ Spree::OrderMailer.class_eval do
     @order = order
     subject = (resend ? "[#{t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{order.number}"
-    mail(to: order.email, subject: subject)
+    mail(to: order.email, subject: subject) do |format|
+      format.text
+      format.html
+    end
   end
 
   def cancel_email(order, resend = false)
     @order = order
     subject = (resend ? "[#{t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{order.number}"
-    mail(to: order.email, subject: subject)
+    mail(to: order.email, subject: subject) do |format|
+      format.text
+      format.html
+    end
   end
 
   def void_email(order, resend = false)
     @order = order
     subject = (resend ? "[#{t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{t('order_mailer.void_email.subject')} ##{order.number}"
-    mail(to: order.email, subject: subject)
+    mail(to: order.email, subject: subject) do |format|
+      format.text
+      format.html
+    end
   end
 
   def capture_email(order, resend = false)
     @order = order
     subject = (resend ? "[#{t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{t('order_mailer.capture_email.subject')} ##{order.number}"
-    mail(to: order.email, subject: subject)
+    mail(to: order.email, subject: subject) do |format|
+      format.text
+      format.html
+    end
   end
 end
